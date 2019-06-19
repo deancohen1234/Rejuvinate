a8fefc72ba0af864ebc6182f2e65b69ac64a39e1
Unknown <dean3712@gmail.com>
2019-06-18 20:50:50 -0600

    Made bezier curve base class

---
+++ Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierCurve
+{
+    public float m_Accuracy = 0.05f;
+
+    private Vector2[] m_CurvePoints;
+
+    public void CreateCurve(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        if (m_Accuracy == 0)
+        {
+            Debug.LogError("Accuracy is Zero for Bezier Creation. Can't divide by 0");
+            return;
+        }
+        //don't know how to properly deallocate memory for array so we just create a new array to destroy previous
+        m_CurvePoints = new Vector2[Mathf.FloorToInt(1.0f / m_Accuracy)];
+
+        for (float f = 0.0f; f <= 1.0f; f += m_Accuracy)
+        {
+            Vector2 point = CalculatePoint(f, p0, p1, p2, p3);
+            int index = Mathf.FloorToInt(f * (float)m_CurvePoints.Length);
+
+            m_CurvePoints[index] = point;
+        }
+    }
+
+    public Vector2 GetPoint(int arrayIndex)
+    {
+        return m_CurvePoints[arrayIndex];
+    }
+
+    public int GetPointArrayLength()
+    {
+        return m_CurvePoints.Length;
+    }
+
+    private Vector2 CalculatePoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        Vector2 partOne = Mathf.Pow((1 - t), 3.0f) * p0;
+        Vector2 partTwo = 3.0f * (Mathf.Pow((1 - t), 2.0f)) * t * p1;
+        Vector2 partThree = 3.0f * (1 - t) * Mathf.Pow(t, 2.0f) * Mathf.Pow(t, 2.0f) * p2;
+        Vector2 partFour = Mathf.Pow(t, 3.0f) * p3;
+
+        Vector2 finalPoint = partOne + partTwo + partThree + partFour;
+
+        return finalPoint;
+    }
+}
---
+++ Assets/Scripts/CurveVisualizer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveVisualizer : MonoBehaviour
+{
+    public Transform[] m_Points;
+
+    private BezierCurve m_Curve;
+    private Vector3 cachedPoint; //used to save memory rather than instantiating new vector3 each frame
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_Curve == null)
+        {
+            m_Curve = new BezierCurve();
+        }
+        else
+        {
+            if (m_Points.Length == 4)
+            {
+                Vector2 p0 = new Vector2(m_Points[0].localPosition.x, m_Points[0].localPosition.y);
+                Vector2 p1 = new Vector2(m_Points[1].localPosition.x, m_Points[1].localPosition.y);
+                Vector2 p2 = new Vector2(m_Points[2].localPosition.x, m_Points[2].localPosition.y);
+                Vector2 p3 = new Vector2(m_Points[3].localPosition.x, m_Points[3].localPosition.y);
+
+                m_Curve.CreateCurve(p0, p1, p2, p3);
+
+                for (int i = 0; i < m_Curve.GetPointArrayLength(); i++)
+                {
+                    Vector2 point = m_Curve.GetPoint(i);
+
+                    cachedPoint = new Vector3(point.x, point.y, 0);
+                    Gizmos.DrawSphere(cachedPoint + transform.position, 0.2f);
+                }
+            }
+        }
+    }
+}
