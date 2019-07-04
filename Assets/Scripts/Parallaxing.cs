c1c0836b2889b01b9703340967cfca0b68e99c26
Unknown <dean3712@gmail.com>
2019-07-04 16:33:38 -0600

    Refactored parallaxing to not be a manager

--- Assets/Scripts/Parallaxing.cs
+++ Assets/Scripts/Parallaxing.cs
@@ -4,44 +4,36 @@ using UnityEngine;
 
 public class Parallaxing : MonoBehaviour
 {
-    public Transform[] m_Backgrounds;
-    private float[] parallaxScales;
-    public float smoothing = 1.0f;
+    public float m_Smoothing = 1.0f;
 
-    private Transform cam;
-    private Vector3 previousCamPos;
+    private Transform m_Camera;
+
+    private Vector3 m_PreviousCamPos;
 
     private void Awake()
     {
-        cam = Camera.main.transform;
+        m_Camera = Camera.main.transform;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        previousCamPos = cam.position;
-
-        parallaxScales = new float[m_Backgrounds.Length];
-        for (int i = 0; i < m_Backgrounds.Length; i++)
-        {
-            parallaxScales[i] = m_Backgrounds[i].position.z * -1;
-        }
+        m_PreviousCamPos = m_Camera.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < m_Backgrounds.Length; i++)
-        {
-            float parallaxX = (previousCamPos.x - cam.position.x) * parallaxScales[i];
-            float parallaxY = (previousCamPos.y - cam.position.y) * parallaxScales[i];
+        float parallaxX = (m_PreviousCamPos.x - m_Camera.position.x) * transform.position.z * -1; //-1 i think because it is moving in opposite direction as cam
+        float parallaxY = (m_PreviousCamPos.y - m_Camera.position.y) * transform.position.z * -1; //-1 i think because it is moving in opposite direction as cam
 
-            float backgroundTargetPosX = m_Backgrounds[i].position.x + parallaxX;
-            float backgroundTargetPosY = m_Backgrounds[i].position.y + parallaxY;
+        float backgroundTargetPosX = transform.position.x + parallaxX;
+        float backgroundTargetPosY = transform.position.y + parallaxY;
 
-            m_Backgrounds[i].position = Vector3.Lerp(m_Backgrounds[i].position, new Vector3(backgroundTargetPosX, backgroundTargetPosY, m_Backgrounds[i].position.z), Time.deltaTime * smoothing);
-        }
+        Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgroundTargetPosY, transform.position.z);
 
-        previousCamPos = cam.position;
+        transform.position = Vector3.Lerp(transform.position, backgroundTargetPos, Time.deltaTime * m_Smoothing);
+
+        m_PreviousCamPos = m_Camera.position;
     }
 }
