9c7f19e4e62f6765dd3f55780c8ee7a5cd896443
Unknown <dean3712@gmail.com>
2019-07-09 18:59:15 -0600

    Added framework for checkpoint system to game

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Checkpoint : MonoBehaviour
+{
+    //order in which checkpoints are obtaining
+    //after obtaining a checkpoint you cannot go backwards and activate previous checkpoint
+    //NOTE "I will deal with ordering later" (Dean)
+    public uint m_Order;
+    public string m_PlayerLayerName = "Player";
+
+    private CheckpointManager.CheckpointEvent m_MainCheckpointEvent;
+    private Vector3 m_Position;
+
+    public void Initialize(CheckpointManager.CheckpointEvent checkpointEvent)
+    {
+        m_MainCheckpointEvent = checkpointEvent;
+
+        m_Position = transform.position;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return m_Position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (m_MainCheckpointEvent != null)
+            {
+                m_MainCheckpointEvent(this);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == m_PlayerLayerName)
+        {
+            //invoke checkpoint for manager
+            m_MainCheckpointEvent(this);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public delegate void CheckpointEvent(Checkpoint checkpoint);
+    public CheckpointEvent m_OnCheckpointActivated;
+
+    public Checkpoint[] m_AllCheckpoints;
+    public PlayerEssenceController m_EssenceController;
+
+    private Checkpoint m_ActiveCheckpoint = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //subscribe manager functions to events
+        m_OnCheckpointActivated += OnCheckpointActivated;
+        m_EssenceController.m_OnPlayerDeath += OnPlayerDeath;
+
+        SetupCheckpoints();
+    }
+
+    private void SetupCheckpoints()
+    {
+        //set so whenever a checkpoint is activated the manager is notified
+        for (int i = 0; i < m_AllCheckpoints.Length; i++)
+        {
+            m_AllCheckpoints[i].Initialize(m_OnCheckpointActivated);
+        }
+    }
+
+    private void OnCheckpointActivated(Checkpoint checkpoint)
+    {
+        Debug.Log("Checkpoint activated + " + checkpoint.m_Order);
+
+        //set active checkpoint
+        m_ActiveCheckpoint = checkpoint;
+    }
+
+    private void OnPlayerDeath()
+    {
+        if (m_ActiveCheckpoint == null)
+        {
+            Debug.Log("Player does not have active checkpoint");
+            return;
+        }
+
+        m_EssenceController.gameObject.transform.position = m_ActiveCheckpoint.GetPosition();
+    }
+}
