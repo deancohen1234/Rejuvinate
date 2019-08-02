582a9c4e7e3ab02dedc51a031b8b3015cc0d7e53
Unknown <dean3712@gmail.com>
2019-08-01 21:54:59 -0600

    Made death zone script

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggering");
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Player")
+        {
+            Debug.Log("Played Died");
+            HealthComponent healthComponent = other.gameObject.GetComponent<HealthComponent>();
+            healthComponent.DealDamage(1000.0f);
+        }
+    }
+}
